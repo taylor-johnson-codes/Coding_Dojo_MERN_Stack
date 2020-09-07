b4d85af0731698aef3186fc0057e655f9699ac7c
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-09-06 23:55:19 -0700

    following demo

--- 2_React/3_Functional_Components/functional_components_instructor_lecture/src/components/Form.js
+++ 2_React/3_Functional_Components/functional_components_instructor_lecture/src/components/Form.js
@@ -2,12 +2,15 @@ import React, {useState} from 'react'
 import EventCard from './EventCard'
 
 const Form = props => {
+
+    const [submitMessage, setSubmitMessage] = useState('');
+
     const [event, setEvent] = useState({
         name: '',
         date: '',
         guests: 0,
         description: ''
-    })
+    });
 
     const changeHandler = e => {
         setEvent({
@@ -16,28 +19,53 @@ const Form = props => {
         })
     }
 
+    const submitHandler = e => {
+        e.preventDefault();  // stops page from reloading
+        
+        // clear the event in form and add a message:
+        setEvent({
+            name: '',
+            date: '',
+            guests: 0,
+            description: ''
+        });
+
+        setSubmitMessage('Thank you for your submission!');
+    }
+
     return (
         <div>
+            {/* Can't use conditional/if statements in React, use ternary operator instead */}
+            {
+                submitMessage != '' ?
+                    <p>{submitMessage}</p>  // if true
+                    :
+                    ''  // if false (can't have nothing after colon)
+            }
+
             {/* taking info from state and passing it to EventCard */}
             <EventCard name={event.name} date={event.date} guests={event.guests} description={event.description}/>
 
             {/* Don't want to redirect form with an action because we don't want the page to reload */}
-            <form>
+            <form onSubmit={submitHandler}>
                 <label htmlFor="name">Name: </label>
-                <input type="text" name="name" onChange={ e => changeHandler(e) }/>
+                <input type="text" name="name" onChange={changeHandler} value={event.name}/>
+                {/* value={} is for clearing the form data after submission */}
                 <br/>
 
                 <label htmlFor="date">Date: </label>
-                <input type="text" name="date" onChange={ e => changeHandler(e) }/>
+                <input type="text" name="date" onChange={changeHandler} value={event.date}/>
                 <br/>
 
                 <label htmlFor="guests">Guests: </label>
-                <input type="text" name="guests" onChange={ e => changeHandler(e) }/>
+                <input type="text" name="guests" onChange={changeHandler} value={event.guests}/>
                 <br/>
 
                 <label htmlFor="description">Description: </label>
-                <input type="text" name="description" onChange={ e => changeHandler(e) }/>
+                <input type="text" name="description" onChange={changeHandler} value={event.description}/>
                 <br/>
+
+                <button type="submit">Submit</button>
             </form>
         </div>
     )
