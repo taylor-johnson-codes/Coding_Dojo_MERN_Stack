3f2e89ad8fe4be65794eab4d4d6f325e93eaaadc
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-09-06 21:23:13 -0700

    following demo

--- 2_React/3_Functional_Components/functional_components_platform_lecture/src/App.js
+++ 2_React/3_Functional_Components/functional_components_platform_lecture/src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <>
-      
-    </>
+    <div>
+      <PersonCard firstName="John" lastName="Smith" age={ 8 } hairColor="Brown" /> 
+      {/* Note: we can pass down a string with or without curly braces. */}
+
+    </div>
   );
 }
 
