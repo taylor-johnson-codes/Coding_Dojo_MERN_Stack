24e05179e24b9a7bed812f6bf445859ce4621d89
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-09-06 22:22:15 -0700

    following demo

---
+++ 2_React/3_Functional_Components/functional_components_instructor_lecture/src/components/EventCard.js
@@ -0,0 +1,14 @@
+// With ES7 React extension, can use rafce snippet
+// rafce = react arrow function component with export
+
+import React from 'react'
+
+const EventCard = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default EventCard
