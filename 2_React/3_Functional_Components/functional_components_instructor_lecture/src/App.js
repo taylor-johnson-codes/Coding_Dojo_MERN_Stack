5d30a0e6e2912f30b39a9c1f711194b1795dea85
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-09-06 22:56:58 -0700

    following demo

--- 2_React/3_Functional_Components/functional_components_instructor_lecture/src/App.js
+++ 2_React/3_Functional_Components/functional_components_instructor_lecture/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './App.css';
+import EventCard from './components/EventCard';
 
 function App() {
   return (
     <div>
-
+      <ul>
+        {/* starting to type EventCard will give an auto import option to import the component above */}
+        <EventCard
+          name={"My Awesome Event"}
+          date={"9/1/2020"}
+          guests={2}
+          description={"Having pizza!"}
+        />
+        <EventCard
+          name={"Car Show"}
+          date={"9/2/2020"}
+          guests={10}
+          description={"Hot rods!"}
+        />
+      </ul>
     </div>
   );
 }
--- 2_React/3_Functional_Components/functional_components_instructor_lecture/src/components/EventCard.js
+++ 2_React/3_Functional_Components/functional_components_instructor_lecture/src/components/EventCard.js
@@ -1,12 +1,29 @@
 // With ES7 React extension, can use rafce snippet
 // rafce = react arrow function component with export
 
-import React from 'react'
+import React, { useState } from 'react'
+
+// The whole thing below is a function so it doesn't have a constructor because constructors are for classes, not functions
 
 const EventCard = props => {
+    // useState has a snippet: const [state, setstate] = useState(initialState)
+    const [name, setName] = useState(props.name);  // don't really need this if you don't plan on changing the item's state (i.e. a person's first name doesn't usually change)
+    const [date, setDate] = useState(props.date);
+    const [guests, setGuests] = useState(props.guests);
+    const [description, setDescription] = useState(props.description);
+    // taken from props and entered into state so it can be changed; props is read-only
+
     return (
         <div>
-            
+            <li>
+                <ul>
+                    <li>Event Name: {name}</li>
+                    <li>Event Date: {date}</li>
+                    <li>Number of Guests: {guests}</li>
+                    <li>Event Description: {description}</li>
+                    <li><button onClick={ () => setGuests(guests+1) }>Add Guest</button></li>
+                </ul>
+            </li>
         </div>
     )
 }
