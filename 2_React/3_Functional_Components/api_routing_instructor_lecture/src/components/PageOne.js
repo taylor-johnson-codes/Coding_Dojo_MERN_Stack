ae48b608c1f8285c1d657a27171f84823477290e
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-09-09 13:04:55 -0700

    following demo

---
+++ 2_React/3_Functional_Components/api_routing_instructor_lecture/src/components/PageOne.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PageOne = (props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PageOne
---
+++ 2_React/3_Functional_Components/api_routing_instructor_lecture/src/components/PageTwo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PageTwo = (props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PageTwo
