bc20dd08f6dcae2b2a3cd0484ee0c5a61f700a31
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-09-10 16:37:22 -0700

    following demo

--- 2_React/4_APIs/api_routing_instructor_lecture/src/App.js
+++ 2_React/4_APIs/api_routing_instructor_lecture/src/App.js
@@ -5,6 +5,7 @@ function App() {
   // const [pokemonName, setPokemonName] = useState("");  // start the state of pokemonName as an empty string
 
   const [pokemon, setPokemon] = useState({}); // using whole pokemon object
+  const [success, setSuccess] = useState(true);
 
   /*
   - useEffect() is a function that takes a callback; callback is the code I want to run given a certain situation
@@ -19,12 +20,19 @@ function App() {
       //.then((response) => console.log(response.name))
       .then((response) => { setPokemon(response) })
       //.catch(error => console.log(error))  // if there's an error in the response, do this
-      .catch(error => setPokemon({name: "ERROR OCCURRED!"}))
+      //.catch(error => setPokemon({name: "ERROR OCCURRED!"}))
+      .catch(error => setSuccess(false))
   }, [])  // leave second parameter (an array) of useEffect function empty if I want this to fire just the first time we get here
 
   return (
     <div>
-      <h1>You got the data for: {pokemon.name}</h1>
+      {
+        success ? 
+          <h1>You got the data for: {pokemon.name}</h1>
+          :
+          <h1>An unexpected error has occurred.</h1>
+
+      }
     </div>
   );
 }
