dcffea80141d76222c6454c0ce88c14b866f5cb7
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-09-14 17:36:41 -0700

    following demo

--- 3_Express/express_instructor_lecture/server/server.js
+++ 3_Express/express_instructor_lecture/server/server.js
@@ -17,3 +17,23 @@ app.use(express.urlencoded({ extended: true }));
 
 They are Express middleware functions, responsible for providing and parsing the request.body data.
 */
+
+
+/*
+server subfolders:
+controllers - will hold all logic for each model (i.e creating, updating, etc...)
+models - will hold all the schemas
+routes - will handle all of our routes for each model
+server.js - will handle all the server logic with express
+*/
+
+
+/*
+import and export JS code/files to other JS code/files:
+
+require("./filePathGoesHere") - used to import from different files
+- The require method automatically looks for JavaScript files, so we don't need to include the .js file extension
+- "./" (dot-slash) is the file path for the current directory (by default, the require() method looks for the modules located in a folder called node_modules)
+
+module.exports = WhateverYouNeedToExportGoesHere - used to export whatever content you need to export from that file
+*/
