8f328bd71f4401b858eca8fb4bec29804bc2b793
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-09-15 20:23:02 -0700

    following demo

--- 4_MongoDB/mongo_db_instructor_lecture/server/server.js
+++ 4_MongoDB/mongo_db_instructor_lecture/server/server.js
@@ -6,6 +6,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(cors);
 
+require("./config/mongoose.config");
 require("./routes/routes")(app);
 
 app.listen(8000, () => console.log("Now listening on port 8000"));
