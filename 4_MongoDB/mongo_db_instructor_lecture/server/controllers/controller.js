55ec2fa480b64f8149b166f6850dd2ffa0a58a3f
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-09-15 20:07:16 -0700

    spun up demo

---
+++ 4_MongoDB/mongo_db_instructor_lecture/server/controllers/controller.js
@@ -0,0 +1,10 @@
+module.exports = { 
+
+    // generic: (req, res) => {
+    //     res.json({ message: "success", data: [1, 2, 3] });
+    // },
+    // newData: (req, res) => {
+    //     console.log(req.body);
+    //     res.json({ message: "success", data: req.body });
+    // }
+}
---
+++ 4_MongoDB/mongo_db_instructor_lecture/server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+// const cors = require("cors");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(cors);
+
+require("./routes/routes")(app);
+
+app.listen(8000, () => console.log("Now listening on port 8000"));
