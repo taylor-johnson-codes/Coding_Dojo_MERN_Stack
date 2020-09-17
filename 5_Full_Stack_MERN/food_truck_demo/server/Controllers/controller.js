d854cad90c152162bbf48632ac319581f69146e4
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-09-17 15:59:09 -0700

    following demo

--- 5_Full_Stack_MERN/food_truck_demo/server/Controllers/controller.js
+++ 5_Full_Stack_MERN/food_truck_demo/server/Controllers/controller.js
@@ -1,32 +1,45 @@
-const Test = require('../models/models');
+const Truck = require('../models/models');
 
 module.exports = {
-    create: (req, res) => {
-        Test.create(req.body)
+    createTruck: (req, res) => {
+        Truck.create(req.body)
             .then(data => res.json(data))
             .catch(err => res.json(err));
     },
 
-    getAll: (req, res) => {
-        Test.find()
+    allTrucks: (req, res) => {
+        Truck.find()
             .then(data => res.json(data))
             .catch(err => res.json(err));
     },
 
-    getOne: (req, res) => {
-        Test.findOne({ _id: req.params.id })
+    oneTruck: (req, res) => {
+        Truck.findOne({ _id: req.params.id })
+            // .then(data => res.json(data))
+            .then(data => {
+                if(data == null){
+                    res.status(500).json({ error: "Food truck doesn't exist." })
+                } else {
+                    res.json(data)
+                }
+            })
+            .catch(err => res.json(err));
+    },
+
+    updateTruck: (req, res) => {
+        Truck.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true, runValidators: true, useFindAndModify: false })
             .then(data => res.json(data))
             .catch(err => res.json(err));
     },
 
-    updateOne: (req, res) => {
-        Test.findOneAndUpdate({ _id: req.params.id }, req.body, { runValidators: true, useFindAndModify: false })
+    addReview: (req, res) => {
+        Truck.
             .then(data => res.json(data))
             .catch(err => res.json(err));
     },
 
     deleteOne: (req, res) => {
-        Test.findOneAndDelete({ _id: req.params.id })
+        Truck.findOneAndDelete({ _id: req.params.id })
             .then(data => res.json(data))
             .catch(err => res.json(err));
     }
--- 5_Full_Stack_MERN/food_truck_demo/server/routes/routes.js
+++ 5_Full_Stack_MERN/food_truck_demo/server/routes/routes.js
@@ -1,9 +1,10 @@
 const controller = require("../controllers/controller");
 
 module.exports = (app) => {
-    app.post("/api/test", controller.create);
-    app.get("/api/test", controller.getAll);
-    app.get("/api/test/:id", controller.getOne);
-    app.patch("/api/test/:id", controller.updateOne);
-    app.delete("/api/test/:id", controller.deleteOne);
+    app.post("/api/trucks", controller.createTruck);
+    app.get("/api/trucks", controller.allTrucks);
+    app.get("/api/trucks/:id", controller.oneTruck);
+    app.patch("/api/trucks/:id", controller.updateTruck);
+    app.patch("/api/trucks/review/:id", controller.addReview);
+    app.delete("/api/trucks/:id", controller.deleteOne);
 };
