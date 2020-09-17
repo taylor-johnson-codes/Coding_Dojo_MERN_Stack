73344ca82cbb0b03181c79aee04f27ae8b7e770d
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-09-16 18:26:02 -0700

    fixed typos

--- 4_MongoDB/mongo_db_instructor_lecture/server/routes/routes.js
+++ 4_MongoDB/mongo_db_instructor_lecture/server/routes/routes.js
@@ -2,15 +2,15 @@ const controller = require("../controllers/controller");
 
 module.exports = app => {
     // Create
-    app.post('/api', controller.create);
+    app.post('/api', controller.create);  // Remember NO () after function names in routes.js!!!!!!!!!
 
     // Read
-    app.get('/api', controller.findAll());
-    app.get('/api/:id', controller.find());
+    app.get('/api', controller.findAll);
+    app.get('/api/:id', controller.find);
 
     // Update
-    app.patch('/api/:id', controller.update());
+    app.patch('/api/:id', controller.update);
 
     // Delete
-    app.delete('/api/:id', controller.delete());
+    app.delete('/api/:id', controller.delete);
 }
--- 4_MongoDB/mongo_db_instructor_lecture/server/server.js
+++ 4_MongoDB/mongo_db_instructor_lecture/server/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-// const cors = require("cors");
+// const cors = require("cors");  // CORS IS NEEDED WHEN WE INCORPORATE FRONT END STUFF (right now it's just back end)
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cors);
+// app.use(cors());  
 
 require("./config/mongoose.config");
 require("./routes/routes")(app);
