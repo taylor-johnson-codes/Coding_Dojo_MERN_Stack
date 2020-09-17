d693ff40f52f067062314680a08bdfa7387a6fd4
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-09-16 23:53:31 -0700

    spun up assignment

---
+++ 5_Full_Stack_MERN/product_manager_assignment/server/config/mongoose.config.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost/product_manager_DB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log("Connection to DB has been established!"))
+    .catch((err) =>
+        console.log("Error occurred when attempting to connect to DB: ", err)
+    );
---
+++ 5_Full_Stack_MERN/product_manager_assignment/server/controllers/controller.js
@@ -0,0 +1,11 @@
+const Test = require('../models/models');
+
+module.exports = {
+    // create: (req, res) => {
+    //     Test.create(req.body)
+    //         .then(data => res.json(data))
+    //         .catch(err => res.json(err));
+    // },
+
+
+};
