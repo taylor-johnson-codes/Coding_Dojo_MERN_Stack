da9947308f842d0d64a8c0ed20359ea380ee47d0
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-09-16 22:52:57 -0700

    following demo

--- 5_Full_Stack_MERN/full_stack_instructor_lecture/client/src/components/AllData.js
+++ 5_Full_Stack_MERN/full_stack_instructor_lecture/client/src/components/AllData.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import {Link} from '@reach/router'
 
-const AllData = () => {
+const AllData = (props) => {
     const [allData, setAllData] = useState([]);
     const [loading, setLoading] = useState(true);
 
@@ -18,6 +18,8 @@ const AllData = () => {
 
     return (
         <div>
+            {/* link will show form when clicked to create a new thing after that componment is created */}
+            <Link to="/new">Add New Thing</Link>
             {
                 loading ?
                 <p>Loading, please wait...</p>
--- 5_Full_Stack_MERN/full_stack_instructor_lecture/client/src/components/NewThing.js
+++ 5_Full_Stack_MERN/full_stack_instructor_lecture/client/src/components/NewThing.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
+// don't need useEffect because when I first get to this page I don't need anything to happen right away
+import axios from "axios";
+import { Link, navigate } from "@reach/router";
+// Link to route to another page; navigate is how to redirect within the single page app from another method
 
-const NewThing = () => {
+const NewThing = (props) => {
+    const [thing, setThing] = useState({
+        name: '',  // keys must match models.js naming
+        email: ''
+    })
+
+    const submitHandler = e => {
+        e.preventDefault();
+    }
+
+    const changeHandler = e => {
+        setThing({
+            ...thing,
+            [e.target.name]: e.target.value
+        });
+    }
+
     return (
         <div>
-            
+            <Link to="/all">Go Back to All Things</Link>
+            <form onSubmit={submitHandler}>
+                <label htmlFor="name">Name: </label>
+                <input type="text" name="name" onChange={changeHandler}/>
+                <br/>
+                <label htmlFor="email">Email: </label>
+                <input type="text" name="email" onChange={changeHandler}/>
+                <br/>
+                <button type="submit">Add Thing</button>
+            </form>
         </div>
     )
 }
