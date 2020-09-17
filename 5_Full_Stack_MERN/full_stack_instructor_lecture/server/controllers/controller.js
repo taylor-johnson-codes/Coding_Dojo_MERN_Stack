6903abe9e99dace2e3b8fcd30a249318590eabb1
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-09-16 21:43:04 -0700

    following demo

--- 5_Full_Stack_MERN/full_stack_instructor_lecture/server/controllers/controller.js
+++ 5_Full_Stack_MERN/full_stack_instructor_lecture/server/controllers/controller.js
@@ -3,11 +3,26 @@ const Test = require('../models/models');
 module.exports = {
     create: (req, res) => {
         Test.create(req.body)
-            .then((data) => res.json(data))
-            .catch((err) => res.json(err));
+            .then(data => res.json(data))
+            .catch(err => res.json(err));
     },
 
-    
+    getAll: (req, res) => {
+        Test.find()
+        /*
+        .then(data => {
+            // if there's no data in the DB
+            if (data.length > 0) {
+                res.json(data)
+            }
+            else {
+                res.status(500).json({ message: "No data in the database" });
+            }
+        })
+        */
+            .then(data => res.json(data))
+            .catch(err => res.json(err));
+    },
 
 
 };
--- 5_Full_Stack_MERN/full_stack_instructor_lecture/server/routes/routes.js
+++ 5_Full_Stack_MERN/full_stack_instructor_lecture/server/routes/routes.js
@@ -1,17 +1,11 @@
 const controller = require("../controllers/controller");
 
 module.exports = (app) => {
-    // C
     app.post("/api/test", controller.create);
-
-    // R
     app.get("/api/test", controller.getAll);
-    app.get("/api/test/:id", controller.getOne);
-
-    // U
-    app.patch("/api/test/:id", controller.updateOne);
-    // OR
-    // app.put("/api/test/:id", controller.updateOne);
-
-    // D
-    app.delete("/api/test/:id", controller.deleteOne);
+    // app.get("/api/test/:id", controller.getOne);
+    // app.patch("/api/test/:id", controller.updateOne);
+    // // OR
+    // // app.put("/api/test/:id", controller.updateOne);
+    // app.delete("/api/test/:id", controller.deleteOne);
+}
