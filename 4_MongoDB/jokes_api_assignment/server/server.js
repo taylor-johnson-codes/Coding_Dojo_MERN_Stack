eb7e5396877baa3ddd758a1512db116fe4732c61
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-09-16 17:31:14 -0700

    working on assignment

--- 4_MongoDB/jokes_api_assignment/server/server.js
+++ 4_MongoDB/jokes_api_assignment/server/server.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const app = express();
-// const cors = require("cors");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cors);
+app.use(cors);
 
 require("./config/mongoose.config");
-// require("./server/config/mongoose.config");
 require("./routes/routes")(app);
-// require("./server/routes/jokes.routes")(app);
 
 app.listen(8000, () => console.log("Now listening on port 8000"));
