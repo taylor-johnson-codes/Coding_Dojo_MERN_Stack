aae672089dba00588704d044ece4e888bd3ad695
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-09-17 17:09:08 -0700

    following demo

--- 5_Full_Stack_MERN/food_truck_demo/client/src/App.js
+++ 5_Full_Stack_MERN/food_truck_demo/client/src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Router, Link} from '@reach/router';
 import './App.css';
 
 function App() {
+  const [activeTab, setActiveTab] = useState("dashboard");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="text-center">Food Trucks</h1>
+      <div className="row">
+        {
+          activeTab === "dashboard" ?
+          <button className="btn btn-secondary col-sm-2">Dashboard</button>
+          : <Link className="col-sm-2" onClick={ () => setActiveTab("dashboard") } to="/"><button className="btn btn-primary">Dashboard</button></Link>
+        }
+
+        {
+          activeTab === "newtruck" ?
+          <button className="btn btn-secondary col-sm-2">New Truck</button>
+          : <Link className="col-sm-2" onClick={ () => setActiveTab("newtruck") } to="/truck/new"><button className="btn btn-primary">New Truck</button></Link>
+        }
+
+        <Router>
+          
+        </Router>
+      </div>
     </div>
   );
 }
