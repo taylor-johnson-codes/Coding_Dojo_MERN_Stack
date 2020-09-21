043ac557a66c3e3177a4b5c37c11900a1d17718e
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-09-21 14:22:17 -0700

    spun up assignment

---
+++ 5_Full_Stack_MERN/authors_assignment/server/config/mongoose.config.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost/authors_DB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log("Connection to DB has been established!"))
+    .catch(err => console.log("Error occurred when attempting to connect to DB: ", err));
